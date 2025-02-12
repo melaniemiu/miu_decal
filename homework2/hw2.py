f229b118eaabab238c51e6a5c7ef969f6af0f494
Melanie Miu <melaniemiu@wifi-10-40-83-5.wifi.berkeley.edu>
2025-02-11 17:04:02 -0800

    adding homework 2

---
+++ homework2/hw2.py
@@ -0,0 +1,20 @@
+# 1) Suppose your current working directory is ~/Desktop/classes/python_decal/. What command would allow you to navigate directly to ~/Desktop?
+    # cd~
+# 2) What does ~/ mean?
+    # it moves one folder to another
+# 3) What is the difference between absolute pathway and relative pathway?
+    # Absolute = the entire pathway to a folder vs relative (how to get to another folder from your current folder)
+# 4) What command would bring you back to your home directory?
+    # cd~
+# 5) If you called rm ./ in your current working directory, what would it do? 
+    # Deletes all files within that directory.
+# 6) In your current working directory, what does calling "git add" do? What about "git commit"? What about "git push"?
+    # git add = adds all modified and new files to the current directory. git commit = captures a snapshot of the changes made to a project. git push = it's copying info to make sure your remote file matches that of the local repository.
+# 7) Let's say you call "git add" in your current working directory and then "git status". What message would appear? What is it telling you?
+    # "changes to be commited" would appear, indicating that those files are ready for the next commit command.
+# 8) What has been the most frustrating error or bug you've encountered in this class so far? How did you troubleshoot and resolve it?
+    # i accidentally made two miu_decal files in my mac and trying to get this homework file into my miu_decal folder was hell because there was another file with the same name :(
+# 9) How does cloning a repository differ from pulling from a repository?
+    # Cloning = copying the remote server repo into my actual computer vs pulling - getting new changes that have been added to the repo into my computer.
+# 10) Tell me a fun fact!
+    # i did not know that there was homework due today...
